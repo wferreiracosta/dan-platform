6e7c3e486f30f029d61a8073c66260659b5d6ded
Wesley Costa <wesferrcosta@gmail.com>
2023-07-23 17:47:28 -0300

    Criando um método GET com @PathVariable id

--- src/main/java/br/com/wferreiracosta/dan/controller/PostController.java
+++ src/main/java/br/com/wferreiracosta/dan/controller/PostController.java
@@ -4,6 +4,7 @@ import br.com.wferreiracosta.dan.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,6 +18,14 @@ public class PostController {
         final var mv = new ModelAndView("posts");
         final var posts = postService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @GetMapping("/posts/{id}")
+    public ModelAndView findDetails(@PathVariable("id") long id) {
+        final var mv = new ModelAndView("postDetails");
+        final var post = postService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 
